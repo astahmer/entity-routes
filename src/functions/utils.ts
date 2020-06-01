9aae205714e57fcb1a702c1683e5594e5cc0646b
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2020-06-01 17:57:17 +0200

    feat(utils): add options arg to override default log options

--- src/functions/utils.ts
+++ src/functions/utils.ts
@@ -1,3 +1,4 @@
 import * as util from "util";
 
-export const log = (obj: Object) => console.dir(util.inspect(obj, { depth: null, colors: true }));
+export const log = (obj: Object, options?: util.InspectOptions) =>
+    console.dir(util.inspect(obj, { depth: 5, colors: true, ...options }));
