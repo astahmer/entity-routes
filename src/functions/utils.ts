4787b6765bc6008fb0afeab67ead8509613f859c
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2020-06-05 14:00:28 +0200

    feat(test): add makeTestCtx & MockContext & change default log depth

--- src/functions/utils.ts
+++ src/functions/utils.ts
@@ -1,4 +1,4 @@
 import * as util from "util";
 
 export const log = (obj: Object, options?: util.InspectOptions) =>
-    console.dir(util.inspect(obj, { depth: 5, colors: true, ...options }));
+    console.dir(util.inspect(obj, { depth: 3, colors: true, ...options }));
--- tests/testConnection.ts
+++ tests/testConnection.ts
@@ -1,4 +1,5 @@
 import { Connection, createConnection } from "typeorm";
+import { QueryParams, Context, deepMerge } from "@/index";
 
 let connection: Connection;
 
@@ -15,3 +16,22 @@ export async function createTestConnection(entities: Function[]) {
 export async function closeTestConnection() {
     await connection?.close();
 }
+
+export const makeTestCtx = <State extends object>(ctx?: Partial<MockContext<State>>) =>
+    deepMerge({}, ctx || {}, {
+        params: {},
+        request: { body: {} },
+        query: {},
+        state: {},
+        status: undefined as number,
+        body: undefined as any,
+    }) as MockContext<State>;
+
+export type MockContext<State extends object = object> = {
+    params: Record<string, string | number>;
+    request: { body: any };
+    query: QueryParams;
+    state: State;
+    status: number;
+    body: any;
+} & Context;
