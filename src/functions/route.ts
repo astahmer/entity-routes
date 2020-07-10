98c5453fd004363cf769f29357a43439d50cdc68
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2020-07-10 12:16:55 +0200

    fix(BridgeRouter): extract areSameRoutes

--- src/functions/route.ts
+++ src/functions/route.ts
@@ -1,4 +1,6 @@
 import { Primitive } from "@/functions/primitives";
+import { BridgeRouterRoute } from "@/router/bridge/BridgeRouter";
+import { isEqualArrays } from "@/functions/array";
 
 export const formatRoutePath = (path: string) => (path[0] === "/" ? path.slice(1) : path);
 
@@ -11,3 +13,9 @@ export const formatRouteName = (path: string, operation: string) =>
  */
 export const parseArrayQS = (query: Record<string, Primitive>, key: string) =>
     query[key + "[]"] ? (Array.isArray(query[key + "[]"]) ? query[key + "[]"] : [query[key + "[]"]]) : query[key];
+
+export function areSameRoutes(routeA: BridgeRouterRoute, routeB: BridgeRouterRoute) {
+    return (
+        routeA.name === routeB.name || (routeA.path === routeB.path && isEqualArrays(routeA.methods, routeB.methods))
+    );
+}
