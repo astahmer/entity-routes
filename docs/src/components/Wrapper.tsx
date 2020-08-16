d97d42a0c770a05340072204ab08b8a1536745e5
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2020-08-17 01:34:38 +0200

    docs(fix/Wrapper): add auto-appended h1 to sidebar

--- docs/src/components/Wrapper.tsx
+++ docs/src/components/Wrapper.tsx
@@ -4,8 +4,15 @@ import { Wrapper as DokzWrapper, MDXComponents } from "dokz";
 // Add h1 to every page based on name(dokz)/title(typedoc)
 export const Wrapper = ({ children, ...props }) => {
     const title = props.meta.title || props.meta.name;
+    const meta = props.meta;
+
+    if (meta?.tableOfContents?.children?.length) {
+        meta.tableOfContents.children[0].slug = "#" + title.toLowerCase();
+        meta.tableOfContents.children[0].title = title;
+    }
+
     return (
-        <DokzWrapper {...props}>
+        <DokzWrapper {...props} meta={meta}>
             <Stack spacing={4}>
                 {title && <MDXComponents.h1>{title}</MDXComponents.h1>}
                 {children}
