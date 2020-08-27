0ff5b16685e3089e26b64cec438761c5ce0487ab
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2020-08-27 23:47:31 +0200

    docs(feat/Wrapper): add guard

--- docs/src/components/Wrapper.tsx
+++ docs/src/components/Wrapper.tsx
@@ -1,12 +1,11 @@
 import { Stack } from "@chakra-ui/core";
 import { Wrapper as DokzWrapper, MDXComponents } from "dokz";
 
-// Add h1 to every page based on name(dokz)/title(typedoc)
 export const Wrapper = ({ children, ...props }) => {
     const title = props.meta.title || props.meta.name;
     const meta = props.meta;
 
-    if (meta?.tableOfContents?.children?.length) {
+    if (title && meta?.tableOfContents?.children?.length) {
         meta.tableOfContents.children[0].slug = "#" + title.toLowerCase();
         meta.tableOfContents.children[0].title = title;
     }
@@ -14,6 +13,7 @@ export const Wrapper = ({ children, ...props }) => {
     return (
         <DokzWrapper {...props} meta={meta}>
             <Stack spacing={4}>
+                {/* Add h1 to every page based on name(dokz)/title(typedoc) */}
                 {title && <MDXComponents.h1 id={title.toLowerCase()}>{title}</MDXComponents.h1>}
                 {children}
             </Stack>
