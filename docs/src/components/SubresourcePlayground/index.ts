10482a9e66b7d712edcbc7077b2e82bd8ed2dc6d
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2020-09-13 23:59:41 +0200

    docs(feat/SubresourcePlayground): add defaultConfig

--- docs/src/components/SubresourcePlayground/index.ts
+++ docs/src/components/SubresourcePlayground/index.ts
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 
+const initialState = require("./defaultConfig.json");
+
 export const SubresourcePlayground = dynamic(
-    () => import("./SubresourcePlayground").then((mod) => mod.SubresourcePlayground),
+    () => import("./SubresourcePlayground").then((mod) => () => mod.SubresourcePlayground({ initialState })),
     { ssr: false }
 );
