88b4e7a2cd019ae4c5422c0437772c2e232b3424
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2020-09-20 23:06:45 +0200

    docs(SideNav): fix active/opened folder style

--- docs/src/components/DokzOverride/SideNav.tsx
+++ docs/src/components/DokzOverride/SideNav.tsx
@@ -7,11 +7,14 @@ import { SideNavProps as DokzSideNavProps } from "dokz/dist/components/SideNav";
 import { CollapseDown } from "./CollapseDown";
 import { CollapseRight } from "./CollapseRight";
 import { ComponentLink } from "dokz/dist/components/NavLink";
+import { useRouter } from "next/router";
 
 // Modified version of SideNav taken from source directly at
 // https://github.com/remorses/dokz/blob/61f9ddbbc923c2b287de86f8ec6e482b8afd4846/dokz/src/components/SideNav.tsx
 type SideNavProps = DokzSideNavProps & { orderingFn?: Function };
 export const SideNav = ({ tree, orderingFn, ...rest }: SideNavProps) => {
+    const router = useRouter();
+
     if (!tree) {
         console.error(new Error(`sidenav tree is null`));
         tree = { name: "", children: [] };
@@ -21,7 +24,7 @@ export const SideNav = ({ tree, orderingFn, ...rest }: SideNavProps) => {
         <Box overflowY="auto" as="nav" aria-label="Main navigation" py="6" px="4" pr="6" {...rest}>
             <Box>
                 {tree.children.map((x) => (
-                    <NavTreeComponent hideDivider key={x.path || x.title} {...x} />
+                    <NavTreeComponent hideDivider key={x.path || x.title} activeRoute={router.pathname} {...x} />
                 ))}
             </Box>
         </Box>
@@ -37,16 +40,25 @@ const NavTreeComponent = ({
     title = "",
     path,
     meta,
-}: DirectoryTree & { depth?: number; hideDivider?: boolean }) => {
+    activeRoute,
+}: DirectoryTree & { depth?: number; hideDivider?: boolean; activeRoute: string }) => {
     const isFolder = !url;
     const formattedTitle = meta ? meta.sidebar_label || meta.title : title || formatTitle(name || "");
     const subTree =
         children &&
         children.map((x) => {
-            return <NavTreeComponent key={x.path || x.title} {...x} depth={depth + 1} />;
+            return <NavTreeComponent key={x.path || x.title} {...x} depth={depth + 1} activeRoute={activeRoute} />;
         });
     if (isFolder && depth > 0) {
-        return <CollapsableTreeNode path={path} depth={depth} title={formattedTitle} subTree={subTree} />;
+        return (
+            <CollapsableTreeNode
+                path={path}
+                depth={depth}
+                title={formattedTitle}
+                subTree={subTree}
+                activeRoute={activeRoute}
+            />
+        );
     }
     if (isFolder) {
         return (
@@ -71,11 +83,21 @@ const NavTreeComponent = ({
     );
 };
 
-function CollapsableTreeNode({ title, path, depth, subTree }) {
-    const { onToggle, isOpen } = useDisclosure();
+function CollapsableTreeNode({ title, path, depth, subTree, activeRoute }) {
+    const isActive = activeRoute.includes(path.replace("src/pages", ""));
+    const { onToggle, isOpen } = useDisclosure(isActive);
+
     return (
         <Stack spacing="0px">
-            <Box display="flex" alignItems="center" cursor="pointer" onClick={onToggle} py="0.2em" my="0.2em">
+            <Box
+                display="flex"
+                alignItems="center"
+                cursor="pointer"
+                onClick={onToggle}
+                py="0.2em"
+                my="0.2em"
+                textDecoration={isActive && "underline"}
+            >
                 <Box
                     mr="0.4em"
                     size="0.6em"
@@ -85,7 +107,15 @@ function CollapsableTreeNode({ title, path, depth, subTree }) {
                 />
                 {title}
             </Box>
-            <Collapse isOpen={isOpen} pl={depth * 20 + "px"}>
+            <Collapse isOpen={isOpen} pl={depth * 20 + "px"} position="relative">
+                <Box
+                    position="absolute"
+                    left="5px"
+                    height="100%"
+                    border="1px solid"
+                    borderColor="gray.400"
+                    opacity={0.1}
+                />
                 {subTree}
             </Collapse>
         </Stack>
