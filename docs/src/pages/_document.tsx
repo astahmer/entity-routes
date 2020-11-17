be409ebc2e5df45bc4d15affac5c3f4d1e3ffbc6
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2020-11-17 11:34:09 +0100

    docs: add ColorModeScript

---
+++ docs/src/pages/_document.tsx
@@ -0,0 +1,24 @@
+import { ColorModeScript } from "@chakra-ui/react";
+import NextDocument, { DocumentContext, Head, Html, Main, NextScript } from "next/document";
+import React from "react";
+
+class Document extends NextDocument {
+    static getInitialProps(ctx: DocumentContext) {
+        return NextDocument.getInitialProps(ctx);
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head />
+                <body>
+                    <ColorModeScript />
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default Document;
