67a082b43de3acdd79263c1df2966c19925c016d
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2020-12-01 12:25:13 +0100

    fix(test-utils): bodyParser include condition

--- packages/test-utils/src/body-parser.ts
+++ packages/test-utils/src/body-parser.ts
@@ -8,12 +8,13 @@ import { TestNextFn } from "./server";
 const methodsWithBody = ["POST", "PUT", "PATCH"];
 /** Ultra basic body parser for very basic testing purpose only, do NOT use in production */
 export async function bodyParser(ctx: TestContext, next: TestNextFn) {
-    if (ctx.req.method in methodsWithBody) {
-        ctx.requestBody = await getRawBody(ctx.req);
+    if (methodsWithBody.includes(ctx.req.method)) {
+        ctx.requestBody = await getRawBody(ctx.req).catch(console.warn);
     }
     next();
 }
 
+/** Only retrieves JSON request body */
 async function getRawBody(req: IncomingMessage): Promise<ObjectLiteral> {
     return new Promise((resolve) => {
         let data = "";
