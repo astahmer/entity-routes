7093c115cc5ff4a790e6037b6bfc20c4fd256dda
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2020-12-01 12:31:13 +0100

    test(test-utils): add server tests

--- packages/test-utils/tests/server.test.ts
+++ packages/test-utils/tests/server.test.ts
@@ -16,4 +16,58 @@ describe("server", () => {
         const result = await getClient(app).request({ url: "/", method: "GET" });
         expect(result.status).toEqual(200);
     });
+
+    it("should set status to 200 when responseBody is set", async () => {
+        const app = createTestServer();
+        app.use(async (ctx) => {
+            ctx.responseBody = "OK";
+        });
+
+        const result = await getClient(app).request({ url: "/", method: "GET" });
+        expect(result.status).toEqual(200);
+    });
+
+    it("should NOT set status to 200 when responseBody is set & a status was already set", async () => {
+        const app = createTestServer();
+        app.use(async (ctx) => {
+            ctx.res.statusCode = 201;
+            ctx.responseBody = "Created";
+        });
+
+        const result = await getClient(app).request({ url: "/", method: "GET" });
+        expect(result.status).toEqual(201);
+    });
+
+    it("should be able to retrieve JSON body from request", async () => {
+        const app = createTestServer();
+        const data = { abc: 123 };
+        app.use(async (ctx) => {
+            ctx.responseBody = ctx.requestBody;
+        });
+
+        const result = await getClient(app).request({ url: "/", method: "POST", data });
+        expect(result.data).toEqual(data);
+    });
+
+    it("should be able to respond with string when responseBody is string", async () => {
+        const app = createTestServer();
+        const data = "OK";
+        app.use(async (ctx) => {
+            ctx.responseBody = data;
+        });
+
+        const result = await getClient(app).request({ url: "/", method: "GET" });
+        expect(result.data).toEqual(data);
+    });
+
+    it("should be able to respond with JSON when responseBody is an ObjectLiteral", async () => {
+        const app = createTestServer();
+        const data = { success: true };
+        app.use(async (ctx) => {
+            ctx.responseBody = data;
+        });
+
+        const result = await getClient(app).request({ url: "/", method: "GET" });
+        expect(result.data).toEqual(data);
+    });
 });
