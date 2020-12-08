42088e71b9406c0736a2fb38192786554b290c81
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2020-12-08 18:21:37 +0100

    refactor: move global.d.ts to root

--- packages/test-utils/tests/global.d.ts
+++
@@ -1,9 +0,0 @@
-import "jest-extended";
-
-declare global {
-    namespace jest {
-        interface Matchers<R> {
-            toEqualMessy(expected: object): CustomMatcherResult;
-        }
-    }
-}
